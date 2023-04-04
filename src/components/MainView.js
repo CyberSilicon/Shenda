2ce9bf6b77b3714690c098d2802ef6cba06ca608
hocine-ie <hocinehoho53@gmail.com>
2023-04-04 03:34:16 +0100

    updated chat ui

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -3,7 +3,7 @@ import React from "react";
 const MainView = () => {
   return (
     <div className="flex flex-1 flex-col w-7/12 h-[100vh] max-sm:hidden border-r border-l justify-between">
-      Map
+      {/* <Typical /> */}
     </div>
   );
 };
